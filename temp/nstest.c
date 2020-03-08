e83c3c7f1aeb7ebdf8493bc125218d64465ffce4
fred-server <wfqq360@163.com>
2020-03-08 17:04:39 +0800

    update temp

--- temp/nstest.c
+++ temp/nstest.c
@@ -7,21 +7,47 @@
 #include <sys/utsname.h>
 #include <unistd.h>
 
-#define STACK_4M    (4 * 1024 * 1024)
+#define STACK_4M (4 * 1024 * 1024)
 
 static char child_stack[STACK_4M];
 
-static int child_func() {
+static int child_func()
+{
 
+    printf("hello6666\n");
     printf("PID: %ld\n", (long)getpid());
     printf("PPID: %ld\n", (long)getppid());
+
+    char *mount_point = "/proc";
+    mkdir(mount_point, 0555);
+    printf("hello6777777\n");
+    printf("hello11111");
+    if (mount("proc", mount_point, "proc", 0, NULL) == -1)
+    {
+        printf("error when mount\n");
+    }
+    printf("hello11111");
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+
+        printf("hello");
+        char *const args[] = {"/bin/bash", "ls"};
+        execv(args[0], args);
+    }
+    else
+    {
+        printf("hello22222");
+        wait(NULL);
+    }
 
     return 0;
 }
 
-int main() {
+int main()
+{
 
-    pid_t child_pid = clone(child_func, child_stack + STACK_4M, SIGCHLD, NULL);
+    pid_t child_pid = clone(child_func, child_stack + STACK_4M,  CLONE_NEWNS | CLONE_NEWPID | CLONE_NEWUSER | SIGCHLD, NULL);
 
     printf("clone() = %ld\n", (long)child_pid);
     waitpid(child_pid, NULL, 0);
